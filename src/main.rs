19343df0269025e14eb86a5eb6422d10a6a61edb
Stein Magnus Jodal <stein.magnus@jodal.no>
2021-09-12 22:40:05 +0200

    Refactor into a struct per electrical component

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{thread, time::Duration};
 
-use gpio_cdev::{Chip, EventRequestFlags, EventType, Line, LineRequestFlags};
+use gpio_cdev::{Chip, EventRequestFlags, EventType, Line, LineHandle, LineRequestFlags};
 
 const PROGRAM_NAME: &str = "garmon";
 
@@ -8,65 +8,90 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{}", PROGRAM_NAME);
 
     let mut chip = Chip::new("/dev/gpiochip0")?;
-    let led_line = chip.get_line(16)?;
-    let led_handle = led_line.request(LineRequestFlags::OUTPUT, 0, PROGRAM_NAME)?;
-    let hc_sr04_trigger_line = chip.get_line(5)?;
-    let hc_sr04_echo_line = chip.get_line(6)?;
+    let led = Led::new(chip.get_line(16)?)?;
+    let hc_sr04 = HcSr04::new(chip.get_line(5)?, chip.get_line(6)?)?;
 
     loop {
-        let distance = read_distance_in_cm(&hc_sr04_trigger_line, &hc_sr04_echo_line)?;
+        let distance = hc_sr04.measure_distance_in_cm()?;
         if distance < 20.0 {
-            led_handle.set_value(1)?;
+            led.on()?;
         } else {
-            led_handle.set_value(0)?;
+            led.off()?;
         }
         thread::sleep(Duration::from_millis(100));
     }
 }
 
-#[allow(dead_code)]
-fn read_distance_in_cm(trigger_line: &Line, echo_line: &Line) -> Result<f64, gpio_cdev::Error> {
-    let trigger_handle = trigger_line.request(LineRequestFlags::OUTPUT, 0, PROGRAM_NAME)?;
 
-    // Trigger sonic burst
-    trigger_handle.set_value(1)?;
-    thread::sleep(Duration::from_micros(10));
-    trigger_handle.set_value(0)?;
-
-    // Measure duration of echo
-    let mut rising_edge_timestamp: Option<u64> = None;
-    for echo_event in echo_line.events(
-        LineRequestFlags::INPUT,
-        EventRequestFlags::BOTH_EDGES,
-        PROGRAM_NAME,
-    )? {
-        let evt = echo_event?;
-        match evt.event_type() {
-            EventType::RisingEdge => {
-                rising_edge_timestamp = Some(evt.timestamp());
-            }
-            EventType::FallingEdge => match rising_edge_timestamp {
-                Some(timestamp) => {
-                    let duration = Duration::from_nanos(evt.timestamp() - timestamp);
-                    let speed_of_sound_in_cm_per_micros = 34_300.0 / 1_000_000.0;
-                    return Ok(duration.as_micros() as f64 * speed_of_sound_in_cm_per_micros / 2.0);
-                }
-                None => {}
-            },
-        }
-    }
-    unreachable!();
+struct Led {
+    handle: LineHandle,
 }
 
-#[allow(dead_code)]
-fn blink_led(led_line: &Line) -> Result<(), gpio_cdev::Error> {
-    let led_handle = led_line.request(LineRequestFlags::OUTPUT, 0, PROGRAM_NAME)?;
-    for i in 1..=10 {
-        println!("Blink {}", i);
-        led_handle.set_value(1)?;
-        thread::sleep(Duration::from_millis(500));
-        led_handle.set_value(0)?;
-        thread::sleep(Duration::from_millis(500));
+impl Led {
+    fn new(line: Line) -> Result<Led, gpio_cdev::Error> {
+        Ok(Led {
+            handle: line.request(LineRequestFlags::OUTPUT, 0, PROGRAM_NAME)?,
+        })
     }
-    Ok(())
+
+    fn on(&self) -> Result<(), gpio_cdev::Error> {
+        self.handle.set_value(1)?;
+        Ok(())
+    }
+
+    fn off(&self) -> Result<(), gpio_cdev::Error> {
+        self.handle.set_value(0)?;
+        Ok(())
+    }
+}
+
+struct HcSr04 {
+    trigger_line: Line,
+    echo_line: Line,
+}
+
+impl HcSr04 {
+    fn new(trigger_line: Line, echo_line: Line) -> Result<HcSr04, gpio_cdev::Error> {
+        Ok(HcSr04 {
+            trigger_line: trigger_line,
+            echo_line: echo_line,
+        })
+    }
+
+    fn measure_distance_in_cm(&self) -> Result<f64, gpio_cdev::Error> {
+        // Trigger sonic burst
+        let trigger = self
+            .trigger_line
+            .request(LineRequestFlags::OUTPUT, 0, PROGRAM_NAME)?;
+        trigger.set_value(1)?;
+        thread::sleep(Duration::from_micros(10));
+        trigger.set_value(0)?;
+
+        // Measure duration of echo
+        let echo_events = self.echo_line.events(
+            LineRequestFlags::INPUT,
+            EventRequestFlags::BOTH_EDGES,
+            PROGRAM_NAME,
+        )?;
+        let mut rising_edge_timestamp: Option<u64> = None;
+        for echo_event in echo_events {
+            let evt = echo_event?;
+            match evt.event_type() {
+                EventType::RisingEdge => {
+                    rising_edge_timestamp = Some(evt.timestamp());
+                }
+                EventType::FallingEdge => match rising_edge_timestamp {
+                    Some(timestamp) => {
+                        let duration = Duration::from_nanos(evt.timestamp() - timestamp);
+                        let speed_of_sound_in_cm_per_micros = 34_300.0 / 1_000_000.0;
+                        return Ok(
+                            duration.as_micros() as f64 * speed_of_sound_in_cm_per_micros / 2.0,
+                        );
+                    }
+                    None => {}
+                },
+            }
+        }
+        unreachable!();
+    }
 }
